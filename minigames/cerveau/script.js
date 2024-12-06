e61bebac3db71850c5761908bb517847bb42ea20
oiercesat <oier.cesat@gmail.com>
2024-12-06 05:45:37 +0100

    Img to public v2

--- minigames/cerveau/script.js
+++ minigames/cerveau/script.js
@@ -57,17 +57,17 @@ let timerInterval;
 
 // Images
 const wallImage = new Image();
-wallImage.src = "public/assets/wall.png";
+wallImage.src = "/assets/wall.png";
 const playerImage = new Image();
-playerImage.src = "public/assets/player.png";
+playerImage.src = "/assets/player.png";
 const resourceImage = new Image();
-resourceImage.src = "public/assets/golden_bubble.png";
+resourceImage.src = "/assets/golden_bubble.png";
 const exitImage = new Image();
-exitImage.src = "public/assets/exit_portal.png";
+exitImage.src = "/assets/exit_portal.png";
 const enemyImage = new Image();
-enemyImage.src = "public/assets/enemy.png";
+enemyImage.src = "/assets/enemy.png";
 const doorImage = new Image();
-doorImage.src = "public/assets/door.png";
+doorImage.src = "/assets/door.png";
 
 
 // Niveaux
